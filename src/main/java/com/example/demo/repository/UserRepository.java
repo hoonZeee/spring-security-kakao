9c417ab7fe421284982cd9b449ee4ef648139d7f
Jihoon <dlwlgns7540@naver.com>
2025-08-23 22:41:37 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.entity.User;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>, IUserRepositoryCustom {
+
+    Optional<User> findById(Integer id);
+
+    Optional<User> findByUsername(String username);
+
+    List<User> findAll();
+
+    User save(User entity);
+
+    void deleteById(Integer id);
+}
